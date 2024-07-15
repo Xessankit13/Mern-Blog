00cca54cadfe5644fad88a118662dfd153d3441c
Xessankit13 <132803543+Xessankit13@users.noreply.github.com>
2024-07-15 20:11:01 +0530

    Page Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
+import { Dashboard } from './pages/Dashboard'
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter className="text-3xl font-bold underline">
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/signin' element={<SignIn/>} />
+      <Route path='/signup' element={<SignUp/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
