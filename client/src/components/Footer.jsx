d55b80e49978c7f3d1e0b1b1e592120333c59313
Xessankit13 <132803543+Xessankit13@users.noreply.github.com>
2024-07-18 20:21:14 +0530

    Sign-in Page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,41 +7,42 @@ export default function FooterCom() {
       <div className='w-full max-w-7xl mx-auto'>
         <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
           <div className='mt-5'>
-            <Link
-              to='/'
-              className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
-            >
-              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                Sahand's
-              </span>
-              Blog
-            </Link>
+          <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-2xl font-bold dark:text-white'
+      >
+        <span className='pl-2 py-1 font-bold rounded-lg text-blue-400'>
+         My
+        </span>
+        <span className='text-blue-600'>Blog</span>
+      </Link>
           </div>
           <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
             <div>
               <Footer.Title title='About' />
               <Footer.LinkGroup col>
-                <Footer.Link
-                  href='https://www.100jsprojects.com'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  100 JS Projects
-                </Footer.Link>
-                <Footer.Link
+              <Footer.Link
                   href='/about'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Sahand's Blog
+                  My Blog
                 </Footer.Link>
+                <Footer.Link
+                  href='#'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Tech Used
+                </Footer.Link>
+              
               </Footer.LinkGroup>
             </div>
             <div>
               <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.github.com/sahandghavidel'
+                  href='https://www.github.com/Xessankit13'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
@@ -63,15 +64,15 @@ export default function FooterCom() {
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <Footer.Copyright
             href='#'
-            by="Sahand's blog"
+            by="My blog"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href='#' icon={BsFacebook}/>
             <Footer.Icon href='#' icon={BsInstagram}/>
             <Footer.Icon href='#' icon={BsTwitter}/>
-            <Footer.Icon href='https://github.com/sahandghavidel' icon={BsGithub}/>
-            <Footer.Icon href='#' icon={BsDribbble}/>
+            <Footer.Icon href='https://github.com/Xessankit13' icon={BsGithub}/>
+            
 
           </div>
         </div>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
-import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export const SignUp = () => {
   const [formData, setFormData] = useState({});
@@ -13,14 +13,14 @@ export const SignUp = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.username || !formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+      return setErrorMessage("Please fill out all fields.");
     }
     try {
       setLoading(true);
       setErrorMessage(null);
-      const res = await fetch('/api/auth/signup', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
@@ -28,8 +28,8 @@ export const SignUp = () => {
         return setErrorMessage(data.message);
       }
       setLoading(false);
-      if(res.ok) {
-        navigate('/sign-in');
+      if (res.ok) {
+        navigate("/sign-in");
       }
     } catch (error) {
       setErrorMessage(error.message);
@@ -37,65 +37,78 @@ export const SignUp = () => {
     }
   };
   return (
-    <div className='min-h-screen mt-20'>
-    <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
-      {/* left */}
-      <div className='flex-1'>
-        <Link to='/' className='font-bold dark:text-white text-4xl'>
-          <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-           My
-          </span>
-          Blog
-        </Link>
-        <p className='text-sm mt-5'>
-          This is a demo project. You can sign up with your email and password
-          or with Google.
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1 ">
+          <Link
+            to="/"
+            className="self-center whitespace-nowrap text-sm sm:text-6xl font-bold dark:text-white"
+          >
+            <span className="pl-2 py-1 font-bold rounded-lg text-blue-400">
+              My
+            </span>
+            <span className="text-blue-600">Blog</span>
+          </Link>
+          <p className='text-sm mt-5 font-medium text-gray-500'>
+        Inspiring insights on tech and lifestyle
         </p>
-      </div>
-      {/* right */}
+        </div>
+        {/* right */}
 
-      <div className='flex-1'>
-        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-          <div>
-            <Label value='Your username' />
-            <TextInput type='text' placeholder='Username' id='username'  onChange={handleChange}/>
-          </div>
-          <div>
-            <Label value='Your email' />
-            <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange} />
-          </div>
-          <div>
-            <Label value='Your password' />
-            <TextInput type='password' placeholder='Password' id='password' onChange={handleChange} />
-          </div>
-          <Button
-              gradientDuoTone='purpleToPink'
-              type='submit'
-              disabled={loading}
-            >
+        <div className="flex-1 ">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="Your username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button color="blue" type="submit" disabled={loading}>
               {loading ? (
                 <>
-                  <Spinner size='sm' />
-                  <span className='pl-3'>Loading...</span>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
                 </>
               ) : (
-                'Sign Up'
+                "Sign Up"
               )}
             </Button>
-        </form>
-        <div className="flex gap-2 text-sm mt-5">
-          <span>Have an account?</span>
-          <Link to='/sign-in' className='text-blue-500'>
-            Sign In
-          </Link>
-        </div>
-        {errorMessage && (
-            <Alert className='mt-5' color='failure'>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
               {errorMessage}
             </Alert>
           )}
+        </div>
       </div>
     </div>
-  </div>
-);
-}
+  );
+};
