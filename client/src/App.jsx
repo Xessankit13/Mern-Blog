e9420b83060f08201813f1e19d2bfc378cf49f7d
Xessankit13 <132803543+Xessankit13@users.noreply.github.com>
2024-07-27 15:42:50 +0530

    User post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,38 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import { Home } from './pages/Home'
-import { About } from './pages/About'
-import { SignIn } from './pages/SignIn'
-import { SignUp } from './pages/SignUp'
-import { Dashboard } from './pages/Dashboard'
-import Header from './components/Header'
-import FooterCom from './components/Footer'
-import PrivateRoute from './components/PrivateRoute';
-import CreatePost from './pages/CreatePost'
-import { Link } from 'react-router-dom';
-import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
-import UpdatePost from './pages/UpdatePost'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { Dashboard } from "./pages/Dashboard";
+import Header from "./components/Header";
+import FooterCom from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
+import CreatePost from "./pages/CreatePost";
+import { Link } from "react-router-dom";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 export default function App() {
   return (
     <BrowserRouter className="text-3xl font-bold underline">
-   <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/sign-in' element={<SignIn/>} />
-      <Route path='/sign-up' element={<SignUp/>} />
-      <Route element={<PrivateRoute />}>
-          <Route path='/dashboard' element={<Dashboard />} />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
-          <Route path='/create-post' element={<CreatePost />} />
-          <Route path='/update-post/:postId' element={<UpdatePost />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
-    </Routes>
-    <FooterCom/>
+       
+        <Route path="/post/:postSlug" element={<PostPage />} />
+      </Routes>
+      <FooterCom />
     </BrowserRouter>
-  )
+  );
 }
