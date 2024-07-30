b97a10c11a0a77b7a6e7b7e332abe73c55a8f782
Xessankit13 <132803543+Xessankit13@users.noreply.github.com>
2024-07-30 19:05:37 +0530

    About me page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      Welcome to MyBlog, your go-to web blogging application designed for
+      writers and creators of all kinds. Whether you're a seasoned blogger or
+      just starting out, MyBlog offers a user-friendly platform to share your
+      stories, ideas, and experiences with the world. With customizable themes,
+      intuitive editing tools, and seamless integration with social media,
+      MyBlog makes it easy to create and manage your blog. Join our community of
+      passionate writers and start your blogging journey today with MyBlog!
+    </div>
+  );
+};
